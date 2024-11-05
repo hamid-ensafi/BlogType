af22a4b85c802241b5ddcb0d07181f7f420879ed
hamid-ensafi <hamidensafi1380.a@gmail.com>
2024-11-06 00:28:57 +0330

    more style header

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -2,7 +2,7 @@ import { FaMountain } from "react-icons/fa6";
 
 export default function Header() {
   return (
-    <header className="flex justify-center items-center text-white ">
+    <header className="flex sticky top-0 justify-center items-center text-white ">
       <div className="w-full 2xl:container bg-dark_500 p-4">
         <div className="flex justify-between">
           <nav>
